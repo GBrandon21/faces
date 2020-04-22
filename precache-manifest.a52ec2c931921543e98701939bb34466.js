6b050de6a58cb26755b9752d1c1c0927d0d6b347
Brandon Guerrero <brandon@Brandons-MacBook-Air.local>
2020-04-22 11:21:45 -0600

    Updates

---
+++ precache-manifest.a52ec2c931921543e98701939bb34466.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af6d77f78a3eb702f021d5b81a9a47c6",
+    "url": "/GBrandon21/facesindex.html"
+  },
+  {
+    "revision": "e1539279f8e399f1bc7f",
+    "url": "/GBrandon21/facesstatic/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "ec690024d81e02c1cc4d",
+    "url": "/GBrandon21/facesstatic/css/main.81718db8.chunk.css"
+  },
+  {
+    "revision": "e1539279f8e399f1bc7f",
+    "url": "/GBrandon21/facesstatic/js/2.be337bd5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/GBrandon21/facesstatic/js/2.be337bd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec690024d81e02c1cc4d",
+    "url": "/GBrandon21/facesstatic/js/main.bc84017f.chunk.js"
+  },
+  {
+    "revision": "87feadd78222207db0cb",
+    "url": "/GBrandon21/facesstatic/js/runtime-main.5e00d8b9.js"
+  }
+]);
